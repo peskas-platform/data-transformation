eea96fe291dd71786d78e6fd6ea2c052e5ecea87
Fernando Cagua <f.cagua@cgiar.org>
2020-09-28 22:04:09 +1300

    Simplify API to contain only one function

--- api.R
+++ api.R
@@ -11,23 +11,16 @@
 
 library(plumber)
 
-#* @apiTitle Pekas data transformation API
-#*
+#* @apiTitle Peskas data transformation API * Transform data from Storage to an
+#analytics BigQuery table so that records can be processed and validated prior
+#to analytics
 
 # Load functions
 purrr::walk(list.files(here::here("R"), full.names = T), source)
 params <- yaml::read_yaml("params.yaml")
 
-#'
-#' @post /transform-pelagic-pubsub
-#' @param message a pub/sub message
-function(message=NULL){
-  googleCloudRunner::cr_plumber_pubsub(message, transform_kobo)
-}
-
-
-#' Transform KOBO survey data
-#' @post /transform-timor-pubsub
+#' Transform data
+#' @post /transform-data-pubsub
 #' @param message a pub/sub message
 function(message=NULL){
 
@@ -37,18 +30,17 @@ function(message=NULL){
   }
 
   # Only continue if object has been created or overwritten
-  if (message$attributes$eventType != "OBJECT_FINALIZE") {
+  if (message$attributes$eventType == "OBJECT_DELETE") {
+    message("Notification of object deleted received")
     return(TRUE)
   }
 
-  this_bucket_datasets <- purrr::keep(params$datasets, ~ .$bucket==message$attributes$bucketId)
-
+  # Check that we know about that dataset
+  this_bucket_datasets <- purrr::keep(
+    .x = params$datasets,
+    .p = ~ .$bucket==message$attributes$bucketId)
   if (length(this_bucket_datasets) == 0)
     stop("This bucket is not supported for auto-transformation")
 
   googleCloudRunner::cr_plumber_pubsub(message, transform_data)
-
 }
-
-
-
